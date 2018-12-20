6275fe7f940249ccf766054abaa3fb407d11d2c3
Sak1361 <0601aiueo@mail.com>
2018-12-20 19:12:29 +0900

    make scraping

--- wakati/statements/scrape_diet2.py
+++ wakati/statements/scrape_diet2.py
@@ -9,8 +9,6 @@ def scrape(path):
     start = '1'
     i = 0
     Reco = ""
-    day = ""
-    chairs = ["大島理森","赤松広隆","伊達忠一","郡司彰"]    #議長リスト
     if os.path.exists(path):
         a = int(input("ファイルあるけど上書きする？:yes(0) or no(1)："))
         if a:
@@ -21,42 +19,39 @@ def scrape(path):
                 f.write("")
 
     while True:
-        #keyword = '麻生太郎'
+        keyword = '安倍晋三'
         startdate = '2018-01-01'
         #enddate = '2018-12-31'
-        maxreco = '100'
-        meeting = '本会議 予算委員会'
+        maxreco = '1'
+        #meeting = '本会議 予算委員会'
         #urllib.parse.quoteが日本語をコーディングしてくれる
-        url = 'http://kokkai.ndl.go.jp/api/1.0/speech?'+urllib.parse.quote('startRecord=' + start
+        url = 'http://kokkai.ndl.go.jp/api/1.0/meeting?'+urllib.parse.quote('startRecord=' + start
                                                                            + '&maximumRecords=' + maxreco
-                                                                           #+ '&speaker=' + keyword
-                                                                           + '&nameOfMeeting=' + meeting
+                                                                           + '&speaker=' + keyword
+                                                                           #+ '&nameOfMeeting=' + meeting
                                                                            + '&from=' + startdate)
                                                                            #+ '&until=' + enddate)
         obj = untangle.parse(url)
+        print(obj.data.numberOfRecords.cdata, type(obj.data.records.record))
         art = obj.data.numberOfRecords.cdata
         for record in obj.data.records.record:
-            name = record.recordData.speechRecord.speaker.cdata
-            if name == '' or name in chairs:    #発言者なしor議長の場合はパス
+            speechrecord = record.recordData.meetingRecord.speechRecord
+            try:
+                print(speechrecord.speech.cdata)
+                Reco += speechrecord.speech.cdata
+            except AttributeError:
                 pass
-            else:
-                speechreco = record.recordData.speechRecord
-                if not day == speechreco.date.cdata:
-                    Reco += speechreco.date.cdata + "\n"            
-                day = speechreco.date.cdata
-                Reco += speechreco.speech.cdata
+                
         Reco += '\n'
-
-        if not i%300:   #300件超えるならここでカキコ
-            with open(path, 'a+') as f:
-                f.write(Reco)
-            Reco = ""
+        with open(path, 'a+') as f:
+            f.write(Reco)
+        Reco = ""
+        i += 5
         try:    #最後にエラーで終わるからここでにゃーん
             start = obj.data.nextRecordPosition.cdata
         except AttributeError:
             print("にゃーんえらー")
             break
-        i += 100
         if i > int(art):
             print("件数到達")
             break
@@ -67,5 +62,3 @@ if __name__ == '__main__':
     path = "demo.csv"
     r = scrape(path)
     # w =カキコ,r =読み,a =追加カキコ,w+ =全部消してカキコ,r+ =既に書かれている内容を上書き,a+ =既に書かれている内容に追記
-    with open(path, 'a+') as f: #残りをかきこ
-        f.write(r)
